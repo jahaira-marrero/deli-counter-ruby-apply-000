d9745f6000e344e7038414a4e26b390b52caa82c
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-11-16 18:28:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,16 @@
+# define the names of the customers and an empty line
+
 name = ["Ada", "Grace", "Kent"]
 katz_deli = []
+
+# define take a number to add the names of the new customers to the empty line and return with a welcome message along with their spot in line.
 
 def take_a_number(katz_deli, name)
    katz_deli << name
    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
  end
  
+ # define who is being served but if there's no one in line it will say that there's no one in line. If there is someone in line then it should say who is being served while removing it.
  def now_serving(katz_deli)
    if katz_deli.length == 0
      puts "There is nobody waiting to be served!"
@@ -14,6 +19,7 @@ def take_a_number(katz_deli, name)
    end
  end
  
+ # Define that actual line with who is being served by name and also include their number in line starting from one. Also include the message.
  def line(katz_deli)
    if katz_deli.length == 0
      puts "The line is currently empty."
